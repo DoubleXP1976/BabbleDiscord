06f7be513fed116beca10db7842d17f44e25af7b
DoubleXP <61705918+DoubleXP1976@users.noreply.github.com>
2020-04-11 21:11:20 -0600

    oops bad indent 724

--- ThetaCog/theta.py
+++ ThetaCog/theta.py
@@ -721,8 +721,8 @@ class Theta(commands.Cog):
 
                                                                 await self.db.theta.set(raw_theta)
 
-                                                           def cog_unload(self):
+                                                            def cog_unload(self):
                                                                 if self.task:
                                                                     self.task.cancel()
 
-                                                          __del__ = cog_unload
+                                                            __del__ = cog_unload
