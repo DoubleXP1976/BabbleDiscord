d5bd1904af1f63a57d0d9577116245c154446500
DoubleXP <61705918+DoubleXP1976@users.noreply.github.com>
2020-04-11 20:42:46 -0600

    another fix to lines 647-659

--- ThetaCog/theta.py
+++ ThetaCog/theta.py
@@ -644,11 +644,11 @@ class Theta(commands.Cog):
                                                                                                         )
 
                                                                                                 m = await channel.send(content, embed=embed)
-                                                                                                    theta._messages_cache.append(m)
-                                                                                                    if edited_roles:
-                                                                                                    for role in edited_roles:
-                                                                                                    await role.edit(mentionable=False)
-                                                                                                    await self.save_theta()
+                                                                                                theta._messages_cache.append(m)
+                                                                                                if edited_roles:
+                                                                                                for role in edited_roles:
+                                                                                                await role.edit(mentionable=False)
+                                                                                                await self.save_theta()
 
                                                             async def _get_mention_str(self, guild: discord.Guild) -> Tuple[str, List[discord.Role]]:
                                                                 """Returns a 2-tuple with the string containing the mentions, and a list of
